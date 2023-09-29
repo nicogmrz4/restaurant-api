e033b879ce760fd7840cb6d482543e28d30d7b08
Nicolás Galarza <nicolasgalarza40@gmail.com>
2023-09-29 16:58:44 -0300

    Customers items per page has been changed

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: CustomerRepository::class)]
-#[ApiResource]
+#[ApiResource(paginationItemsPerPage: 50)]
 class Customer
 {
     #[ORM\Id]
